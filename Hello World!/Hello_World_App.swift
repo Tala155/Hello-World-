e6c119e34590642da305612a17e489ea0485a5c8
Tala155 <talaabdullah2017@hotmail.com>
2024-10-21 15:12:07 +0300

    Initial Commit

---
+++ Hello World!/Hello_World_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_App.swift
+//  Hello World!
+//
+//  Created by Tala Abdullah on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello World!Tests/Hello_World_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_Tests.swift
+//  Hello World!Tests
+//
+//  Created by Tala Abdullah on 18/04/1446 AH.
+//
+
+import Testing
+@testable import Hello_World_
+
+struct Hello_World_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
